020c3373650f46c0cbae26753440cfdf6a49f1a4
sacha0914 <sacha0914.poidevin@gmail.com>
2023-04-23 19:53:14 +0200

    [FRONT] Ajouter le package react navigation

---
+++ src/navigations/TabNavigator.tsx
@@ -0,0 +1,14 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../screens/Home';
+
+const Tab = createBottomTabNavigator();
+
+export default function TabNavigator() {
+  return (
+    <Tab.Navigator>
+        <Tab.Screen name='Home' component={Home} />
+    </Tab.Navigator>
+  )
+}
