c0b9e96861eeb545a2bad2fac9c0a0cdf22074d9
JulienBois <71647803+JulienBois@users.noreply.github.com>
2023-04-26 13:03:54 +0200

    Ajout des différents fonts et background

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,7 +1,9 @@
-import {StyleSheet, View} from 'react-native'
-import {TextInput, Text, Button} from 'react-native-paper'
+import {ImageBackground, StyleSheet, View} from 'react-native'
+import {Text, Button, TextInput} from 'react-native-paper'
 import React from 'react'
 import {Controller, useForm} from "react-hook-form";
+import Animated, {FadeInUp, FadeInDown} from "react-native-reanimated";
+import AnimatedView from "react-native-reanimated/lib/types/lib/reanimated2/component/View";
 
 // @ts-ignore
 export default function SignUp({navigation}) {
@@ -16,35 +18,73 @@ export default function SignUp({navigation}) {
         }
     })
 
+
     const onSubmit = (data: { mail: string, password: string }) => {
         navigation.navigate("Tab");
     };
+
+    const styles = StyleSheet.create({
+        container: {
+            flex: 2,
+            flexDirection: 'column',
+            gap: 40,
+            alignItems: 'center',
+        },
+        input: {
+        },
+        connection: {
+            width: '70%'
+        },
+        image: {
+            flex: 1,
+            justifyContent: 'center'
+        },
+        rounded: {
+            width: '70%',
+            borderRadius: 25,
+            overflow: 'hidden',
+            borderStyle: 'solid',
+            borderColor: '#0000',
+        }
+    })
+
     return (
-        <View>
-            <Text variant='headlineMedium'>Page de connexion</Text>
-            <Controller rules={{required: true}}
-                        control={control}
-                        render={({field: {onChange, onBlur, value}}) => (
-                            <TextInput
-                                placeholder="Adresse mail"
-                                onBlur={onBlur}
-                                onChangeText={onChange}
-                                value={value}
-                            />
-                        )} name="mail"/>
-            {errors.mail && <Text>Le mail est obligatoire pour se connecter</Text>}
-            <Controller rules={{required: true}}
-                        name="password"
-                        control={control}
-                        render={({field: {onChange, onBlur, value}}) => (
-                            <TextInput placeholder="Mot de passe"
-                                       onBlur={onBlur}
-                                       onChangeText={onChange}
-                                       value={value}
-                                       secureTextEntry={true}/>
-                        )}/>
-            {errors.password && <Text>Le mot de passe est obligatoire pour se connecter</Text>}
-            <Button onPress={handleSubmit(onSubmit)}>Se connecter</Button>
-        </View>
+        <ImageBackground style={styles.image} source={require('./../../assets/images/background-gradient-phone.png')}
+                         resizeMode='cover'>
+            <View style={styles.container}>
+                <Animated.Text entering={FadeInDown} exiting={FadeInUp}>Page de connexion</Animated.Text>
+                <View style={styles.rounded}>
+                    <Controller rules={{required: true}}
+                                control={control}
+                                render={({field: {onChange, onBlur, value}}) => (
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder="Adresse mail"
+                                        onBlur={onBlur}
+                                        onChangeText={onChange}
+                                        value={value}
+                                    />
+                                )} name="mail"/>
+                </View>
+                {errors.mail && <Text>Le mail est obligatoire pour se connecter</Text>}
+                <View style={styles.rounded}>
+                    <Controller rules={{required: true}}
+                                name="password"
+                                control={control}
+                                render={({field: {onChange, onBlur, value}}) => (
+                                    <TextInput
+                                        style={styles.input}
+                                        placeholder="Mot de passe"
+                                        onBlur={onBlur}
+                                        onChangeText={onChange}
+                                        value={value}
+                                        secureTextEntry={true}/>
+                                )}/>
+                </View>
+                {errors.password && <Text>Le mot de passe est obligatoire pour se connecter</Text>}
+                <Button style={styles.connection} onPress={handleSubmit(onSubmit)} mode='contained'>Se
+                    connecter</Button>
+            </View>
+        </ImageBackground>
     )
 }
