ea0a2beb56ecde4a701d6b62726ec3ee5c9c5810
JulienBois <71647803+JulienBois@users.noreply.github.com>
2023-04-26 19:13:21 +0200

    Création de la page de Connexion avec import de la librairie material et gestion venue dans le menu (#2)

--- App.tsx
+++ App.tsx
@@ -1,14 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import {StatusBar} from 'expo-status-bar';
+import React from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import {Provider as PaperProvider} from 'react-native-paper';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 import AppNavigator from './src/navigations/AppNavigator';
 
 export default function App() {
-  return (
-    <SafeAreaProvider>
-      <AppNavigator />
-    </SafeAreaProvider>
-  );
+    return (
+        <SafeAreaProvider>
+            <PaperProvider>
+                <AppNavigator/>
+            </PaperProvider>
+        </SafeAreaProvider>
+    );
 }
 
 
