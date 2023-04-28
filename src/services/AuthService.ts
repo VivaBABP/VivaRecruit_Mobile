648ecb1fc1048ec0706c81df20e09a43651dd91b
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-04-28 17:20:20 +0200

    ajout screen pour test le refresh token (à enlever pour les futurs commits)

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,15 +1,21 @@
 import { CreateUserDTO } from './../client/recruitBack';
 import { URL } from "@env";
 import { AuthClient, CredentialDTO, TokenDTO } from "../client/recruitBack";
+import axiosApiInstance from '../configuration/axiosInstance';
 
 export class AuthService {
   private authClient = new AuthClient(URL)
 
-  signIn(credential: CredentialDTO): Promise<TokenDTO> {
+  signIn(credential: CredentialDTO): Promise<TokenDTO> {  
     return this.authClient.signIn(credential);
   }
 
   signUp(createUser: CreateUserDTO): Promise<TokenDTO> {
     return this.authClient.signUp(createUser);
+  }
+
+  test(): Promise<string> {
+    const testClient = new AuthClient(URL, axiosApiInstance)
+    return testClient.test();
   }
 }
