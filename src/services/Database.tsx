8096a296bdcdaa030e6241d8b37878e900fff90d
Gianni <72388911+GianniB77@users.noreply.github.com>
2023-05-06 16:58:38 +0200

    VIV-53 Passage de la page contact form à update (#7)
    * enlever console log inutile
    
    * contact form deviens update
    
    ---------
    
    Co-authored-by: Gianni <GianniB77@users.noreply.github.com>

--- src/services/Database.tsx
+++ src/services/Database.tsx
@@ -3,6 +3,8 @@ import { ResultSet, SQLResultSet, WebSQLDatabase } from "expo-sqlite";
 
 export var db = SQLite.openDatabase('test.db','2');
 
+
+// à ajouter après la validation du compte, n'oublie pas de d'ajouter aussi la fonction create candidat
 export function createTableInfoUser(): any {
     return new Promise((resolve, reject) => {
         db.transaction((tx) => {
@@ -30,10 +32,10 @@ export async function getInfoUsers(): Promise<SQLResultSet> {
     })
 }
 
-export function getInfoUsersByMail(mail: string) {
+export function getInfoUsersByMail(infosUser: { mail: string, nom: string, prenom: string, phoneNumber: string, lastDiploma: string }) {
     return new Promise((resolve, reject) => {
         db.transaction((tx) => {
-            tx.executeSql('SELECT mail,nom, prenom, phoneNumber, lastDiploma from infoUser where mail = ?1', [mail], (transaction, resultSet) => {
+            tx.executeSql('SELECT mail,nom, prenom, phoneNumber, lastDiploma from infoUser where mail = ?1', [infosUser.mail], (transaction, resultSet) => {
                 resolve(resultSet);
             }), (transaction: any, error: any) => {
                 reject(error);
@@ -46,6 +48,33 @@ export function insertInfoUser(infosUser: { mail: string, nom: string, prenom: s
     return new Promise((resolve, reject) => {
         db.transaction((tx) => {
             tx.executeSql('INSERT INTO infoUser(mail, nom, prenom, phoneNumber, lastDiploma) VALUES (?1,?2,?3,?4,?5)', [infosUser.mail, infosUser.nom, infosUser.prenom, infosUser.phoneNumber, infosUser.lastDiploma],
+                (transaction, resultSet) => {
+                    resolve(resultSet);
+                }), (transaction: any, error: any) => {
+                reject(error);
+            }
+        })
+    })
+}
+
+// à ajouter lors de la validation du compte après la création de la table.
+export function createInfoUserCandidat(infosUser: { mail: string }) {
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql('INSERT INTO infoUser(mail) VALUES (?)', [infosUser.mail],
+                (transaction, resultSet) => {
+                    resolve(resultSet);
+                }), (transaction: any, error: any) => {
+                reject(error);
+            }
+        })
+    })
+}
+
+export function updateInfoUser(infosUser: { mail: string, nom: string, prenom: string, phoneNumber: string, lastDiploma: string }) {
+    return new Promise((resolve, reject) => {
+        db.transaction((tx) => {
+            tx.executeSql('UPDATE infoUser SET mail = ?1, nom = ?2, prenom = ?3, phoneNumber = ?4, lastDiploma = ?5 WHERE id = ?6', [infosUser.mail, infosUser.nom, infosUser.prenom, infosUser.phoneNumber, infosUser.lastDiploma, 1],
                 (transaction, resultSet) => {
                     resolve(resultSet);
                 }), (transaction: any, error: any) => {
