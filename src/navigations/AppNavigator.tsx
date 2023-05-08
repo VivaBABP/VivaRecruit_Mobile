f77c11fc9e7f7718a2c2e55122c29c548cd29894
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-05-08 10:57:37 +0200

    Tab Bar custom + indexation

--- src/navigations/AppNavigator.tsx
+++ src/navigations/AppNavigator.tsx
@@ -1,12 +1,9 @@
-import { View, Text } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 import TabNavigator from './TabNavigator';
 import SignIn from "../screens/SignIn";
-import Home from "../screens/Home";
 import SignUp from "../screens/SignUp"
-import CV from "../screens/CV";
 import ActivationCode from '../screens/ActivationCode';
 
 const Stack = createNativeStackNavigator();
@@ -15,10 +12,10 @@ export default function AppNavigator() {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName='SignIn'>
-                <Stack.Screen name='SignIn' component={SignIn} options={{headerShown: true}}/>
-                <Stack.Screen name='Tab' component={TabNavigator} options={{headerShown: false}}/>
-                <Stack.Screen name='SignUp' component={SignUp} options={{headerShown: true}}/>
-                <Stack.Screen name='ActivationCode' component={ActivationCode} options={{headerShown: true}}/>
+                <Stack.Screen name='SignIn' component={SignIn} />
+                <Stack.Screen name='Tab' component={TabNavigator} options={{ headerShown: false }} />
+                <Stack.Screen name='SignUp' component={SignUp} />
+                <Stack.Screen name='ActivationCode' component={ActivationCode} />
             </Stack.Navigator>
         </NavigationContainer>
     )
