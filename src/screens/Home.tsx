7a899c9f650fe4dd66591074fed8b0ac58824e3d
JulienBois <71647803+JulienBois@users.noreply.github.com>
2023-05-11 09:23:52 +0200

    intégration de la méthode de disconnect sur la page home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,12 +1,16 @@
 import { View, Text, Button } from 'react-native'
-import React from 'react'
+import React, {useContext} from 'react'
+import {AuthContext} from "../context/AuthContext";
 
 // @ts-ignore
 export default function Home({ navigation }) {
+
+    const { disconnect } = useContext(AuthContext);
+
     return (
         <View>
             <Text>Bonjour Hugo</Text>
-            <Button title="Déconnecter" onPress={() => navigation.popToTop()} />
+            <Button title="Déconnecter" onPress={() => disconnect()} />
         </View>
     )
 }
