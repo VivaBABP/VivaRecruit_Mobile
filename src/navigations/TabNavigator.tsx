cc33acfc967e178aa8b34c2c471543ee9d0d4496
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-05-11 09:28:29 +0200

    VIV-68 Customisation de la Tab Bar (V1) (#9)
    * utilisation du serveur dev
    
    * Tab Bar custom + indexation

--- src/navigations/TabNavigator.tsx
+++ src/navigations/TabNavigator.tsx
@@ -4,32 +4,63 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Home from '../screens/Home';
 import Contact_Form from '../screens/ContactForm';
 import Job_Form from '../screens/JobForm';
-import RefreshTest from '../screens/RefreshTest';
 import CV from "../screens/CV";
-import SaveInfo from "../screens/ContactForm";
 import PageQrCode from "../screens/PageQrCode";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigator() {
-  return (
-    <Tab.Navigator initialRouteName="Home">
-        <Tab.Screen name='Home' component={Home} options={{
-            tabBarIcon: ({color, size}) => ( <Ionicons name="home" color={color} size={size}/>)
-        }} />
-        <Tab.Screen name='Contact Form' component={Contact_Form} options={{
-            tabBarIcon: ({color, size}) => ( <Ionicons name="document-text" color={color} size={size}/>)
-        }} />
-        <Tab.Screen name='Job Form' component={Job_Form} options={{
-            tabBarIcon: ({color, size}) => ( <Ionicons name="file-tray-full" color={color} size={size}/>)
-        }} />
-        <Tab.Screen name='RefreshTest' component={RefreshTest} />
-        <Tab.Screen name='Cv' component={CV} options={{
-            tabBarIcon: ({color, size}) => ( <Ionicons name="reader" color={color} size={size}/>)
-        }} />
-        <Tab.Screen name='QrCode' component={PageQrCode} options={{
-            tabBarIcon: ({color, size}) => ( <Ionicons name="qr-code-outline" color={color} size={size}/>)
-        }} />
-    </Tab.Navigator>
-  )
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            screenOptions={{
+                tabBarShowLabel: false,
+                tabBarStyle: {
+                    position: 'absolute',
+                    bottom: 25,
+                    right: 20,
+                    left: 20,
+                    borderRadius: 20,
+                }
+            }}
+            sceneContainerStyle={{
+                marginBottom: 70
+            }}
+        >
+            <Tab.Screen
+                name='Home'
+                component={Home}
+                options={{
+                    tabBarIcon: ({ color, size }) => (<Ionicons name="home" color={color} size={size} />)
+                }}
+            />
+            <Tab.Screen
+                name='Contact Form'
+                component={Contact_Form}
+                options={{
+                    tabBarIcon: ({ color, size }) => (<Ionicons name="document-text" color={color} size={size} />)
+                }}
+            />
+            <Tab.Screen
+                name='Job Form'
+                component={Job_Form}
+                options={{
+                    tabBarIcon: ({ color, size }) => (<Ionicons name="file-tray-full" color={color} size={size} />)
+                }}
+            />
+            <Tab.Screen
+                name='Cv'
+                component={CV}
+                options={{
+                    tabBarIcon: ({ color, size }) => (<Ionicons name="reader" color={color} size={size} />)
+                }} />
+            <Tab.Screen
+                name='QrCode'
+                component={PageQrCode}
+                options={{
+                    tabBarIcon: ({ color, size }) => (<Ionicons name="qr-code-outline" color={color} size={size} />)
+                }}
+            />
+        </Tab.Navigator>
+    )
 }
