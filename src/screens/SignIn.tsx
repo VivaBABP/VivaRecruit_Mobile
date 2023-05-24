9b7103d33657991d5a04b5e64102ef122302cb25
Gianni <72388911+GianniB77@users.noreply.github.com>
2023-05-24 23:07:28 +0200

    gh

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,4 +1,4 @@
-import { Image, StyleSheet, View } from 'react-native'
+import { Image, ScrollView, StyleSheet, View } from 'react-native'
 import { Text, Button, TextInput } from 'react-native-paper'
 import React, { useContext, useState } from 'react'
 import { Controller, useForm } from "react-hook-form";
@@ -31,14 +31,12 @@ export default function SignIn({ navigation }) {
         container: {
             flex: 2,
             flexDirection: 'column',
-            alignItems: 'center',
         },
         input: {
-            marginBottom: 60,
+            marginBottom: 30,
             display: 'flex',
             alignItems: 'flex-start',
             padding: 0,
-            borderColor: '#00ccff'
         },
         connection: {
             width: '70%',
@@ -46,68 +44,76 @@ export default function SignIn({ navigation }) {
         },
         image: {
             flex: 1,
-            resizeMode: 'center',
             width: 250,
             height: 200,
-            marginTop: 80
+            marginTop: 50
         },
         inputs: {
             width: '60%',
             overflow: 'hidden',
             borderStyle: 'solid',
             borderColor: '#0000',
+        },
+        button: {
+            marginBottom: 30,
+        },
+        align: {
+            alignItems: 'center'
         }
     })
 
     return (
-        <View style={styles.container}>
-            <Text style={{fontFamily: '700'}}>Test Police</Text>
-            <Image
-                style={styles.image}
-                source={require('./../../assets/logo/adaptive-icon_vivatech.png')}
-            />
-            <View style={styles.inputs}>
-                <Controller rules={{
-                    required: true,
-                    pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/, message: 'Email invalide' }
-                }}
-                    control={control}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            keyboardType={'email-address'}
-                            style={styles.input}
-                            placeholder="Adresse mail"
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                            autoComplete={'email'}
-                            inputMode={'email'}
-                            mode='outlined'
-                            label="mail"
-                            underlineColor='#00ccff'
-                        />
-                    )} name="email" />
+        <ScrollView style={styles.container}>
+            <View style={styles.align}>
+                {/* <Text style={{fontFamily: '700'}}>Test Police</Text> */}
+                <Image
+                    style={styles.image}
+                    source={require('./../../assets/logo/adaptive-icon_vivatech.png')}
+                />
+                <View style={styles.inputs}>
+                    <Controller rules={{
+                        required: true,
+                        pattern: { value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/, message: 'Email invalide' }
+                    }}
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                keyboardType={'email-address'}
+                                style={styles.input}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                autoComplete={'email'}
+                                inputMode={'email'}
+                                mode='outlined'
+                                placeholder="Adresse mail"
+                                
+                            />
+                        )} name="email" />
+                </View>
+                <View style={styles.inputs}>
+                    <Controller rules={{ required: true }}
+                        name="password"
+                        control={control}
+                        render={({ field: { onChange, onBlur, value } }) => (
+                            <TextInput
+                                style={styles.input}
+                                placeholder="Mot de passe"
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                                secureTextEntry={true}
+                                mode='outlined' />
+                        )} />
+                </View>
+                {(errors.password || errors.email) ? <Text>Champs obligatoires invalide</Text> : <Text>{error}</Text>}
+                <View style={styles.connection}>
+                    <Button style={styles.button} onPress={handleSubmit(onSubmit)} mode='contained'>Se
+                        connecter</Button>
+                    <Button style={styles.button} onPress={() => navigation.navigate("SignUp")} mode='contained'>S'inscrire
+                    </Button>
+                </View>
             </View>
-            <View style={styles.inputs}>
-                <Controller rules={{ required: true }}
-                    name="password"
-                    control={control}
-                    render={({ field: { onChange, onBlur, value } }) => (
-                        <TextInput
-                            style={styles.input}
-                            placeholder="Mot de passe"
-                            onBlur={onBlur}
-                            onChangeText={onChange}
-                            value={value}
-                            secureTextEntry={true}
-                            mode='outlined' />
-                    )} />
-            </View>
-            {(errors.password || errors.email) ? <Text>Champs obligatoires invalide</Text> : <Text>{error}</Text>}
-            <Button style={styles.connection} onPress={handleSubmit(onSubmit)} mode='contained'>Se
-                connecter</Button>
-            <Button style={styles.connection} onPress={() => navigation.navigate("SignUp")} mode='contained'>S'inscrire
-            </Button>
-        </View>
+        </ScrollView>
     )
 }
