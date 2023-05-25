55c7b5d8614ad6dad195681947ea5a64a59e0d4c
Hugo Araujo <66556677+huhulacolle@users.noreply.github.com>
2023-05-25 21:58:38 +0200

    formulaire d'entreprise

--- src/screens/CompanyForm.tsx
+++ src/screens/CompanyForm.tsx
@@ -1,13 +1,13 @@
-import {Text, View, StyleSheet, ScrollView, Switch} from "react-native";
+import { Text, View, StyleSheet, ScrollView, Switch } from "react-native";
 import { Controller, useForm } from "react-hook-form";
 import { Button, TextInput } from "react-native-paper";
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 import Constants from "expo-constants";
-import {CreateCompanyDto, UpdateCompanyDto} from "../client/recruitBack";
-import {PaperSelect} from "react-native-paper-select";
-import {CompanyService} from "../services/CompanyService";
-import {ListItem} from "react-native-paper-select/lib/typescript/interface/paperSelect.interface";
-import {CompanyTypeService} from "../services/CompanyTypeService";
+import { CreateCompanyDto, UpdateCompanyDto } from "../client/recruitBack";
+import { PaperSelect } from "react-native-paper-select";
+import { CompanyService } from "../services/CompanyService";
+import { ListItem } from "react-native-paper-select/lib/typescript/interface/paperSelect.interface";
+import { CompanyTypeService } from "../services/CompanyTypeService";
 
 // Ce formulaire est le formulaire de mise à jour de donnée de contact seulement pour les candidats. 
 // La ligne sera créée lors de la création du compte avec le mail uniquement.
@@ -31,9 +31,9 @@ export default function CompanyForm() {
 
     const getCompanies = () => {
         companies.pop();
-        companyService.findAll().then((res) =>{
-            res.forEach((e) =>{
-                companies.push({_id: e.idCompany.toString(), value: e.companyName});
+        companyService.findAll().then((res) => {
+            res.forEach((e) => {
+                companies.push({ _id: e.idCompany.toString(), value: e.companyName });
             });
         }).catch((err) => {
             console.log(JSON.stringify(err));
@@ -42,10 +42,10 @@ export default function CompanyForm() {
     }
 
     const getTypeCompanies = () => {
-        companyTypeService.getCompanyTypes().then((res) =>{
+        companyTypeService.getCompanyTypes().then((res) => {
             typeCompanies.pop();
             res.forEach((e) => {
-                typeCompanies.push({_id: e.idCompanyType.toString(), value: e.companyTypeLabel})
+                typeCompanies.push({ _id: e.idCompanyType.toString(), value: e.companyTypeLabel })
             })
         })
     };
@@ -66,9 +66,9 @@ export default function CompanyForm() {
         setTypeCompanySelected(id.selectedList.at(0)._id as string);
     }
     const save = (data: UpdateCompanyDto) => {
-        if (!isSwitchOn){
-            companyService.setCompany(idCompany, {companyTypeId: data.companyTypeId, companyName: data.companyName, websiteLink: data.websiteLink, lineOfBusiness: data.lineOfBusiness, description: data.description} as UpdateCompanyDto)
-                .then(() => {alert("Entreprise bien modifiée")}).catch((err) => alert(JSON.stringify(err)));
+        if (!isSwitchOn) {
+            companyService.setCompany(idCompany, { companyTypeId: data.companyTypeId, companyName: data.companyName, websiteLink: data.websiteLink, lineOfBusiness: data.lineOfBusiness, description: data.description } as UpdateCompanyDto)
+                .then(() => { alert("Entreprise bien modifiée") }).catch((err) => alert(JSON.stringify(err)));
         } else {
             console.log(typeCompanySelected);
             const company = {
@@ -90,41 +90,13 @@ export default function CompanyForm() {
         }
     };
 
-    const styles = StyleSheet.create({
-        container: {
-            flex: 1,
-            paddingTop: Constants.statusBarHeight,
-            padding: 8,
-            backgroundColor: 'White',
-        },
-        label: {
-            color: '#000000',
-            margin: 20,
-            marginLeft: 0,
-        },
-        button: {
-            marginTop: 20,
-            marginBottom: 70,
-            color: 'white',
-            height: 40,
-            backgroundColor: 'White',
-            borderRadius: 4,
-        },
-        input: {
-            backgroundColor: 'white',
-            borderColor: '#000000',
-            borderBottomWidth: 2,
-            height: 40,
-            padding: 10,
-            borderRadius: 4,
-        },
-    })
-
     return (
         <ScrollView style={styles.container}>
-            <Text>Créer une entreprise :</Text>
-            <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
-            {!isSwitchOn && <PaperSelect label="Entreprise" arrayList={companies} selectedArrayList={companies} multiEnable={false} value={companySelected} onSelection={(id: any) => setForm(id)}/>}
+            {!isSwitchOn && <PaperSelect textInputStyle={styles.input} containerStyle={{ marginTop: 20 }} label="Entreprise" arrayList={companies} selectedArrayList={companies} multiEnable={false} value={companySelected} onSelection={(id: any) => setForm(id)} />}
+            <View style={{ flex: 2, flexDirection: 'row', }}>
+                <Text style={styles.createCompany}>Créer une entreprise :</Text>
+                <Switch value={isSwitchOn} onValueChange={onToggleSwitch} />
+            </View>
             <Text style={styles.label}>Nom de l'entreprise</Text>
             <Controller
                 control={control}
@@ -137,7 +109,7 @@ export default function CompanyForm() {
                     />
                 )}
                 name="companyName"
-                rules={{required: true}}
+                rules={{ required: true }}
             />
             {errors.CompanyName && <Text>Nom requis</Text>}
             <Text style={styles.label}>Description</Text>
@@ -183,27 +155,68 @@ export default function CompanyForm() {
             <Controller
                 control={control}
                 render={({ field: { onChange, onBlur, value } }) => (
-                    <PaperSelect label="Type" arrayList={typeCompanies} selectedArrayList={typeCompanies} multiEnable={false} value={typeCompanySelected} onSelection={(id: any) => onChange(changeTypeCompany(id))}/>
-            )}
+                    <PaperSelect label="Type" textInputStyle={styles.input} arrayList={typeCompanies} selectedArrayList={typeCompanies} multiEnable={false} value={typeCompanySelected} onSelection={(id: any) => onChange(changeTypeCompany(id))} />
+                )}
                 name="companyType"
             />
-            <View style={styles.button}>
-                <Button
-                    onPress={() => {
-                        reset({
-                            CompanyName: '',
-                            Description: '',
-                            WebsiteLink: '',
-                            LineOfBusiness: '',
-                            CompanyType: ''
-                        })
-                    }}
-                >Reset</Button>
-
-                <Button
-                    onPress={handleSubmit(save)}
-                >Valider</Button>
-            </View>
+            <Button
+                mode="contained"
+                style={styles.button}
+                onPress={handleSubmit(save)}
+            >
+                Valider
+            </Button>
         </ScrollView>
     )
 }
+
+const styles = StyleSheet.create({
+    label: {
+        fontFamily: '700',
+        alignSelf: 'center',
+        color: '#000000',
+        margin: 20,
+        marginLeft: 0,
+    },
+    createCompany: {
+        fontFamily: '700',
+        alignSelf: 'center',
+        color: '#000000',
+        margin: 20,
+        marginLeft: '25%',
+    },
+    connection: {
+        width: '70%',
+        marginBottom: 50
+    },
+    button: {
+        margin: 30,
+        backgroundColor: '#EC4D0C',
+        fontFamily: '700'
+
+    },
+    container: {
+        flex: 1,
+        fontFamily: '700'
+    },
+    input: {
+        // width: '60%',
+        marginRight: '10%',
+        marginLeft: '10%',
+        // height: 60,
+        overflow: 'hidden',
+        borderStyle: 'solid',
+        borderColor: '#0000',
+    },
+    align: {
+        alignItems: 'center'
+    },
+    inputDescription: {
+        width: '60%',
+        height: 100,
+        overflow: 'hidden',
+        borderStyle: 'solid',
+        borderColor: '#0000',
+
+    }
+});
